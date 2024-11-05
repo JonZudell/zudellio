cb6accef42f52ea4546c0755c7de1d239de11eec
Jon Zudell <jonathon.zudell@gmail.com>
2024-11-04 18:12:35 -0600

    focus bruh

--- src/components/input/PasswordInput.tsx
+++ src/components/input/PasswordInput.tsx
@@ -25,20 +25,23 @@ interface PasswordInputProps {
 const PasswordInput = forwardRef<HTMLInputElement, PasswordInputProps>(
   ({ value, id, name, setter, className, label = 'Password' }, ref) => {
     const [showPassword, setShowPassword] = useState(false);
+    const [focusedOnEye, setFocusedOnEye] = useState(false);
 
     return (
       <div className={`relative mt-2 ${className}`}>
         <label htmlFor={id} className="block mb-2">
           {label}
         </label>
-        <div className="flex items-center standard-shadow">
+        <div
+          className={`flex items-center border standard-shadow focus-within:border-blue-500`}
+        >
           <input
             ref={ref}
             type={showPassword ? 'text' : 'password'}
             id={id}
             name={name}
             value={value}
-            className={`p-2 border textinput flex-grow background-color`}
+            className={`p-2 border textinput flex-grow background-color border-0 focus:border-blue-500`}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
               setter(e.target.value)
             }
@@ -54,9 +57,15 @@ const PasswordInput = forwardRef<HTMLInputElement, PasswordInputProps>(
           <span
             id={`${id}-toggle`}
             role="button" // Role for the toggle button
-            className="background-color inline-flex items-center font-semibold text-gray-900 border-0 hover:bg-gray-800 h-full"
+            className={`background-color inline-flex items-center font-semibold text-gray-900 border-0 hover:bg-gray-800 h-full`}
             onClick={() => setShowPassword(!showPassword)}
             tabIndex={0} // Make the span tabbable
+            onFocus={() => {
+              setFocusedOnEye(true);
+            }}
+            onBlur={() => {
+              setFocusedOnEye(false);
+            }}
             onKeyDown={(e) => {
               if (e.key === 'Enter' || e.key === ' ') {
                 setShowPassword(!showPassword);
@@ -66,8 +75,10 @@ const PasswordInput = forwardRef<HTMLInputElement, PasswordInputProps>(
           >
             <FontAwesomeIcon
               icon={showPassword ? faEyeSlash : faEye}
-              className="m-3 h-4 w-4"
-              style={{ color: '#D3D3D3' }} // Set the color here
+              className={`m-3 h-4 w-4`}
+              style={{
+                color: `${focusedOnEye ? 'rgb(96 165 250)' : '#d3d3d3'}`,
+              }} // Set the color here
             />
           </span>
         </div>
